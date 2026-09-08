de67486e90b6e20ef025dd5f178fae8d8df2e199
Andres <Andres@DESKTOP-59TSBQV.home>
2026-09-08 17:35:34 +0200

    Ejemplos básicos

---
+++ bases/src/escobar/andres/formacion/ejemplos/LeerNumeros.java
@@ -0,0 +1,17 @@
+package escobar.andres.formacion.ejemplos;
+
+import java.util.Scanner;
+
+public class LeerNumeros {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Dime un número y te lo multiplico por 2: ");
+		
+		int numero = sc.nextInt();
+		
+		System.out.println(numero * 2);
+		
+		sc.close();
+	}
+}
